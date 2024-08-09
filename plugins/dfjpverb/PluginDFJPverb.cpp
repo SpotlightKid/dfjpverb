02b4d40e67a9a0dcdf6e0ff26dc9e4c7d416d40e
Christopher Arndt <chris@chrisarndt.de>
2024-08-09 10:35:31 +0200

    fix: update deprecated parameter hint name
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- plugins/dfjpverb/PluginDFJPverb.cpp
+++ plugins/dfjpverb/PluginDFJPverb.cpp
@@ -44,7 +44,7 @@ void PluginDFJPverb::initParameter(uint32_t index, Parameter& parameter) {
     parameter.ranges.min = range->min;
     parameter.ranges.max = range->max;
     parameter.ranges.def = range->init;
-    parameter.hints = kParameterIsAutomable;
+    parameter.hints = kParameterIsAutomatable;
 
     if (dsp->parameter_is_boolean(index))
         parameter.hints |= kParameterIsBoolean;
